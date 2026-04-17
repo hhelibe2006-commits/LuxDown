4ce6b1255fe5555997a4ba277af0f7a181218fa4
hhhhhhhhhhh2003 <hhhhhhhhhh2003@outlook.com>
2026-04-17 14:56:32 +0800

    设置窗口居中

--- src/utils/centered_ui.py
+++ src/utils/centered_ui.py
@@ -3,6 +3,9 @@ from PySide6.QtWidgets import QMainWindow, QApplication
 from PySide6.QtGui import QScreen
 
 def center_ui(window):
+    #获取屏幕中心点
     screen = QtGui.QGuiApplication.primaryScreen().availableGeometry()
+    #获取窗口大小
     size = window.geometry()
+    #设置窗口位置
     window.move((screen.width() - size.width())/2, (screen.height() - size.height())/2)
