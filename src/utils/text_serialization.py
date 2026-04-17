f6d922caeecb7dfaa382a944fd1010a3c633ed59
hhhhhhhhhhh2003 <hhhhhhhhhh2003@outlook.com>
2026-04-17 14:58:01 +0800

    将多条链接序列化

--- src/utils/text_serialization.py
+++ src/utils/text_serialization.py
@@ -1,3 +1,4 @@
-def text_to_dict(text):
-    text = text.toPlainText()
-    return text.splitlines()
+from PySide6.QtWidgets import QPlainTextEdit
+def text_to_dict(text : QPlainTextEdit):
+    urls = text.toPlainText()
+    return urls.splitlines()
