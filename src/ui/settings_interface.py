a9426503aef0b3bdc91ca492c782baba500b6567
hhhhhhhhhhh2003 <hhhhhhhhhh2003@outlook.com>
2026-04-18 15:17:45 +0800

    设置界面

--- src/ui/settings_interface.py
+++ src/ui/settings_interface.py
@@ -1,53 +1,66 @@
-from PySide6.QtWidgets import QDialog, QLabel, QLineEdit, QCheckBox, QVBoxLayout, QHBoxLayout, \
+from PySide6.QtWidgets import QDialog, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, \
     QPushButton, QFileDialog, QGroupBox, QComboBox, QSizePolicy
+
 from utils import set_window_size, center_ui
 
 class SettingsInterface(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.layout = QVBoxLayout(self)
+        self.vbox = QVBoxLayout(self)
         self.path_input = QLineEdit()
-        self.group_box1 = QGroupBox("音频")
-        self.group_box2 = QGroupBox("视频")
-        self.combo1 = QComboBox()
-        self.combo2 = QComboBox()
-        self.path_input.setPlaceholderText("下载路径")
+        self.audio_box = QGroupBox("音频")
+        self.video_box = QGroupBox("视频")
+        self.audio_combobox = QComboBox()
+        self.video_combobox = QComboBox()
         self.path_button = QPushButton("选择路径")
-        self.path_button.clicked.connect(self.choos_dir)
-        self.path_input.setText("C:\\Users\\hhhhh\\Downloads")
-        self.initialize_ui()
+        self.initialize()
 
-    def initialize_ui(self):
+    def initialize(self):
+        self.initialize_path()
+        self.initialize_download_content()
+        self.initialize_format()
+        self.initialize_windows()
+
+    def initialize_windows(self):
         self.setWindowTitle("设置")
-        self.group_box1.setCheckable(True)
-        self.group_box2.setCheckable(True)
-        self.combo1.addItems(["mp3"])
-        self.combo2.addItems(["mp4"])
-        self.combo1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
-        self.combo2.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
-        set_window_size(self, 0.6)
+        set_window_size(self, percentage = 0.6)
         center_ui(self)
+        self.vbox.addStretch()
+
+    def initialize_path(self):
+        self.path_input.setPlaceholderText("下载路径")
+        self.path_button.clicked.connect(self.choose_dir)
+        self.path_input.setText("C:\\Users\\hhhhh\\Downloads")
         hbox = QHBoxLayout()
         hbox.addWidget(self.path_input)
         hbox.addWidget(self.path_button)
-        self.layout.addWidget(QLabel("下载路径:"))
-        self.layout.addLayout(hbox)
+        self.vbox.addWidget(QLabel("下载路径:"))
+        self.vbox.addLayout(hbox)
+
+    def initialize_download_content(self):
+        self.audio_box.setCheckable(True)
+        self.video_box.setCheckable(True)
         hbox = QHBoxLayout()
-        hbox.addWidget(self.group_box1)
-        hbox.addWidget(self.group_box2)
-        self.layout.addWidget(QLabel("下载内容:"))
-        self.layout.addLayout(hbox)
-        self.layout.addWidget(QLabel("格式选择"))
+        hbox.addWidget(self.audio_box)
+        hbox.addWidget(self.video_box)
+        self.vbox.addWidget(QLabel("下载内容:"))
+        self.vbox.addLayout(hbox)
+
+    def initialize_format(self):
+        self.audio_combobox.addItems(["mp3"])
+        self.video_combobox.addItems(["mp4"])
+        self.audio_combobox.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.video_combobox.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.vbox.addWidget(QLabel("格式选择"))
         hbox = QHBoxLayout()
         hbox.addWidget(QLabel("音频:"))
-        hbox.addWidget(self.combo1)
+        hbox.addWidget(self.audio_combobox)
         hbox.addWidget(QLabel("视频:"))
-        hbox.addWidget(self.combo2)
-        self.layout.addLayout(hbox)
-        self.layout.addStretch()
+        hbox.addWidget(self.video_combobox)
+        self.vbox.addLayout(hbox)
 
-    def choos_dir(self):
-        dir_path = QFileDialog.getExistingDirectory(self,"选择文件夹", self.path_input.text())
+    def choose_dir(self):
+        dir_path = QFileDialog.getExistingDirectory(self, "选择文件夹", self.path_input.text())
         if dir_path:
             self.path_input.setText(dir_path)
 
