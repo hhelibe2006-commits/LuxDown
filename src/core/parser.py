da9b1a3d2b041c0ce5497543bf03a1ef2323e1a4
hhelibe2006-commits <hhelibe2006@gmail.com>
2026-04-20 20:46:37 +0800

    解析函数

--- src/core/parser.py
+++ src/core/parser.py
@@ -1,5 +1,12 @@
-import yt_dlp
-
-class Parser:
-    def __init__(self):
-        pass
+from yt_dlp import YoutubeDL
+def parse(url):
+    with YoutubeDL() as ydl:
+        info = ydl.extract_info(url, download=False)
+        list = ['title', 'description', 'id', 'ext', 'duration_string', 'filesize_approx', 'webpage_url', 'thumbnail']
+        llist = []
+        if info.get("_type") != None:
+            for i in info.get("entries"):
+                llist.append({j:k for j,k in i.items() if j in list})
+        else:
+            llist.append({j:k for j,k in info.items() if j in list})
+        return llist
