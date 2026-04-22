b22c87dabf1d68dc372aa70b2b11420f18e1a64f
hhelibe2006-commits <hhelibe2006@gmail.com>
2026-04-22 09:47:05 +0800

    完善了解析类

--- src/core/parser.py
+++ src/core/parser.py
@@ -1,14 +1,16 @@
 from yt_dlp import YoutubeDL
-
+from typing import Any
 def parse(url):
     with YoutubeDL() as ydl:
-        info = ydl.extract_info(url, download=False)
+        info:Any = ydl.extract_info(url, download=False)
+        if not info:
+            return []
         clist = ['title', 'id', 'description', 'ext', 'duration_string', 'filesize_approx', 'webpage_url']
-        llist = [{info['thumbnail']}]
-        if info.get("_type") is not None:
-            for i in info.get("entries"):
+        llist = []
+        if info.get('_type') is not None:
+            for i in info.get('entries'):
                 llist.append({j:k for j,k in i.items() if j in clist})
         else:
             llist.append({j:k for j,k in info.items() if j in clist})
         print(llist)
-        return llist
+        return llist,info.get('title'),info.get('thumbnail')
