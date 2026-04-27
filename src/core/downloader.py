af6e1f1a58f76181479d75cdcb21724d58c62ecb
hhelibe2006-commits <hhelibe2006@gmail.com>
2026-04-27 19:44:54 +0800

    将下载逻辑改为函数

--- src/core/downloader.py
+++ src/core/downloader.py
@@ -1,6 +1,10 @@
 """
 该文件存放调用yt-dlp进行下载的函数与类
 """
-class Downloader:
-    def __init__(self):
-        pass
+import yt_dlp
+def download(url, hook):
+    ydl_opts = {
+        "outtmpl": "C:\\Users\\hhhhh\\Downloads\\%(title)s.%(ext)",
+        'progress_hooks': [hook]}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
