9108d8134fb04663c077bb0e622fbd69835b8b37
hhelibe2006-commits <hhelibe2006@gmail.com>
2026-04-29 20:05:02 +0800

    补全了下载逻辑

--- src/core/downloader.py
+++ src/core/downloader.py
@@ -2,9 +2,28 @@
 该文件存放调用yt-dlp进行下载的函数与类
 """
 import yt_dlp
-def download(url, hook):
+
+
+def download(url, progress_hook, index, settings):
     ydl_opts = {
-        "outtmpl": "C:\\Users\\hhhhh\\Downloads\\%(title)s.%(ext)s",
-        'progress_hooks': [hook]}
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        "outtmpl": f"{settings.path_input}\\{index}-%(title)s.%(ext)s",
+        'progress_hooks': [progress_hook],
+        'max_sleep_interval': 30,
+        #'quiet': True,
+        #'no_color': True,
+    }
+    if settings.on_audio and settings.on_video:
+        ydl_opts['format'] = 'bestvideo+bestaudio/best'
+        ydl_opts['merge_output_format'] = settings.video1
+
+    elif settings.on_audio:
+        ydl_opts['format'] = 'bestaudio/best'
+        ydl_opts['merge_output_format'] = settings.audio1
+    elif settings.on_video:
+        ydl_opts['format'] = 'bestvideo'
+        ydl_opts['merge_output_format'] = settings.video1
+    else:
+        ydl_opts['format'] = ''
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl: # type: ignore
         ydl.download([url])
