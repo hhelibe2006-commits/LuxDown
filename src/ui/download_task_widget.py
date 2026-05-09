34684e4f6033f8047cd4710a9ae9b561652bc5b3
hhelibe2006-commits <hhelibe2006@gmail.com>
2026-05-09 19:38:26 +0800

    将DownloadTaskWidget类单独拆分

---
+++ src/ui/download_task_widget.py
@@ -0,0 +1,43 @@
+import threading
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QLabel, QProgressBar, QPushButton
+from PySide6.QtCore import Slot
+from ui.main_interface import SignalEmitter
+
+
+class DownloadTaskWidget(QWidget):
+    def __init__(self, signal, title):
+        super().__init__()
+        self._cancel_event = threading.Event()
+        self.list_item = None
+        self._is_dual_download = False
+        self.external_emitter = signal
+        self.emitter = SignalEmitter()
+        self.hbox = QHBoxLayout()
+        self.label = QLabel(title)
+        self.hbox.addWidget(self.label)
+        self.progress_bar = QProgressBar()
+        self.progress_bar.setValue(0)
+        self.hbox.addWidget(self.progress_bar)
+        self.button = QPushButton(self.tr("取消"))
+        self.hbox.addWidget(self.button)
+        self.setLayout(self.hbox)
+        self.button.clicked.connect(self.on_cancel_clicked)
+        self.emitter.progress_update.connect(self.update_progress)
+
+
+    @Slot()
+    def on_cancel_clicked(self):
+        self._cancel_event.set()
+
+    def update_progress(self, value):
+        self.progress_bar.setValue(value)
+
+    def progress_hook(self, d):
+        if self._cancel_event.is_set():
+            raise
+        if d['status'] == 'downloading':
+            self.emitter.progress_update.emit(d['_percent'])
+        elif d['status'] == 'finished':
+            if not self._is_dual_download:
+                self.external_emitter.download_finished.emit(self.list_item)
+            self._is_dual_download = False
