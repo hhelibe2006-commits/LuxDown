0a2cb3f6a3ed1c2b56f17870958ff22deabcb042
hhelibe2006-commits <hhelibe2006@gmail.com>
2026-05-13 21:15:21 +0800

    添加英文支持

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@
 """
 import sys
 
+from PySide6.QtCore import QTranslator
 # pylint: disable=no-name-in-module
 from PySide6.QtGui import QIcon
 # pylint: disable=no-name-in-module
@@ -18,6 +19,9 @@ def main() -> None:
     进行主界面的创建与显示，并指定应用图标
     """
     app=QApplication(sys.argv)
+    translator = QTranslator()
+    #if translator.load("zh_CN.qm"):
+    #    app.installTranslator( translator)
     apply_stylesheet(app, theme="dark_cyan.xml", extra = {'density_scale' : '0'})
     windows=MainInterface()
     windows.setWindowIcon(QIcon("LuxDown.png"))
