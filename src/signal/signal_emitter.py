13c8279560c2f9c974e42cfd7947e9f938eca9f4
hhelibe2006-commits <hhelibe2006@gmail.com>
2026-05-16 16:32:07 +0800

    显式标注了信号的类型

--- src/signal/signal_emitter.py
+++ src/signal/signal_emitter.py
@@ -1,9 +1,10 @@
 from PySide6.QtCore import QObject, Signal
+from PySide6.QtWidgets import QListWidgetItem
 
 
 class SignalEmitter(QObject):
     parse_finished : Signal = Signal(tuple)
     download_start : Signal = Signal(object, object, object)
-    progress_update : Signal = Signal(object)
-    download_finished : Signal = Signal(object)
-    check_update : Signal = Signal(object, object)
+    progress_update : Signal = Signal(int)
+    download_finished : Signal = Signal(QListWidgetItem)
+    check_update : Signal = Signal(str, str)
