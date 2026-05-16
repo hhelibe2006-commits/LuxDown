08c938ac79dda25edf5b8f530e5245e5f8a0c4fe
hhelibe2006-commits <hhelibe2006@gmail.com>
2026-05-16 17:29:35 +0800

    优化了多语言函数

--- src/utils/locale.py
+++ src/utils/locale.py
@@ -6,12 +6,23 @@ from PySide6.QtWidgets import QApplication
 
 def on_locale(app : QApplication):
     translator = QTranslator()
-    olocale = {'zh_CN' : 'zh_CN.qm', 'en_US' : 'en.qm', 'en_GB' : 'en.qm', 'de_DE' : 'de.qm', 'fr_FR' : 'fr.qm', 'es_ES' : 'es.qm'}
-    lang_code, _ = locale.getdefaultlocale()
-    if olocale.get(lang_code):
+    olocale = {
+        'zh_CN': 'zh_CN.qm',
+        'en_US': 'en.qm',
+        'en_GB': 'en.qm',
+        'de_DE': 'de.qm',
+        'fr_FR': 'fr.qm',
+        'es_ES': 'es.qm'
+    }
+
+    default = locale.getdefaultlocale()
+    lang_code = default[0] if default and default[0] else None
+
+    if lang_code and olocale.get(lang_code):
         if translator.load(olocale[lang_code]):
             app.installTranslator(translator)
+            return
 
-    else:
-        if translator.load('en.qm'):
-            app.installTranslator(translator)
+    #当找不到语言时用英语
+    if translator.load('en.qm'):
+        app.installTranslator(translator)
