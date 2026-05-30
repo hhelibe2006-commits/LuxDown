4a3b56ba968067f6e9419202f107c7cca0e6936f
hhelibe2006-commits <hhelibe2006@gmail.com>
2026-05-30 14:57:01 +0800

    冒充chrome

--- src/core/parser.py
+++ src/core/parser.py
@@ -21,6 +21,7 @@ def extract_info(
         "logger": logger,
         "max_sleep_interval": 30,
         "cookiefile": cookies_file,
+        'impersonate': 'chrome',
     }
     if platform.system() == "Windows":
         ydl_opts["deno_path"] = os.path.join("deno", "deno.exe")
--- src/core/ydl_options.py
+++ src/core/ydl_options.py
@@ -28,6 +28,7 @@ def build_ydl_opts(
         "fragment_retries": 3,
         "cookiefile": settings.cookies_file,
         "format_sort": [f"res:{resolution.split('x')[-1]}"],
+        'impersonate': 'chrome',
     }
 
     if settings.download_audio and settings.download_video:
